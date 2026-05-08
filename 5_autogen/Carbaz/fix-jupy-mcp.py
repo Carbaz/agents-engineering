f30371f43ea8cb03882155b616c3fc2bbdf80896
Carlos Bazaga <cabazaga@gmail.com>
2026-05-08 22:54:08 +0200

    Add Jupyter compatibility fix for MCP server creation on Windows

---
+++ 5_autogen/Carbaz/fix-jupy-mcp.py
@@ -0,0 +1,26 @@
+"""Windows + Jupyter fix for MCP server creation."""
+
+# Extra imports to make this and next cells work standalone
+from IPython.display import display, Markdown
+from dotenv import load_dotenv
+
+# Jupyter on Windows has an issue with MCP stdio client due to how Jupyter captures
+# "stderr".
+#   See: https://github.com/modelcontextprotocol/python-sdk/issues/1103
+
+# The MCP client internally uses "stdio_client()" to create a server process and Jupyter
+# replaces "stderr" with a Python object (ipykernel.iostream.OutStream) that can't
+# provide a real OS file descriptor, so "subprocess.Popen()" crashes when trying to use
+# it.
+
+# When "stderr=None" is passed instead of Jupyter's captured "stderr", the subprocess
+# can start correctly.
+#   Fix: https://github.com/modelcontextprotocol/python-sdk/issues/1103#issuecomment-3470416291
+
+# We are not directly calling the "stdio_client" so we need to "monkey patch" it
+# to pass "None" for "stderr" always.
+from mcp.client import stdio
+stdio._original_stdio_client = stdio.stdio_client
+stdio.stdio_client = lambda server, *_: stdio._original_stdio_client(server, None)
+
+load_dotenv(override=True)
